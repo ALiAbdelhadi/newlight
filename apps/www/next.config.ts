6ad9c41715623edf3474c731d42fb36e6e3dc9e5
Ali Abdelhadi <aliabdelhadi010@gmail.com>
2025-12-12 15:58:56 +0200

    update project

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -3,32 +3,20 @@ import createNextIntlPlugin from 'next-intl/plugin';
 import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
-  compress: true,
-  poweredByHeader: false,
   reactStrictMode: true,
   transpilePackages: ["@repo/database"],
   compiler: {
     styledComponents: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-  },
-
+  compress: true,
+  poweredByHeader: false,
   webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        '@prisma/client': 'commonjs @prisma/client',
-      });
-    }
-
     config.resolve.extensionAlias = {
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
     };
-
     if (!config.resolve.extensions) {
       config.resolve.extensions = [];
     }
-
     config.resolve.extensions = [
       '.ts',
       '.tsx',
@@ -38,10 +26,8 @@ const nextConfig: NextConfig = {
         !['.ts', '.tsx', '.js', '.jsx'].includes(ext)
       ),
     ];
-
     return config;
   },
-
   images: {
     remotePatterns: [
       {
@@ -62,31 +48,6 @@ config = withPWA({
   sw: "sw.js",
   fallbacks: {
     document: "/offline",
-  },
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: false,
-  reloadOnOnline: true,
-  workboxOptions: {
-    disableDevLogs: true,
-    exclude: [
-      /\.map$/,
-      /^manifest.*\.js$/,
-      /icons\/.*$/,
-      /_buildManifest\.js$/,
-      /_ssgManifest\.js$/,
-    ],
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
   },
 })(config);
 
