900a6dd02dc16a613f45c2aa934f323972d3c8d3
Ali Abdelhadi <aliabdelhadi010@gmail.com>
2025-12-24 15:30:46 +0200

    update admin

--- apps/admin/app/(auth)/layout.tsx
+++ apps/admin/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     return (
         <>
             {children}
-            <div className="fixed top-4 right-4 container">
+            <div className="relative top-4 right-4 container">
                 <ThemeToggle />
             </div>
         </>
